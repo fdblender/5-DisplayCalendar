6810862b2811df72576c92a06a9275e8f8bf0602
fdblender <fdblender@gmail.com>
2016-06-17 09:09:33 -0400

    fixed calendar program

--- src/DisplayCalendar.java
+++ src/DisplayCalendar.java
@@ -1,60 +1,56 @@
-import java.util.Date;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Scanner;
-import java.util.SimpleTimeZone;
-import java.util.TimeZone;
+import java.util.Locale;
 
 public class DisplayCalendar {
 
 	public static void main(String[] args) {
+		int month = 5;
+		int year = 2016;
+		String monthStr, yearStr;
 		Scanner scan = new Scanner(System.in);
-		int month, year;
-		String months[] = {"Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov", "Dec"};
-		String daysOfWeek[] = {" Sun", " Mon", "Tues", " Wed", "Thurs", " Fri", " Sat"};
+		
 		System.out.print("Enter the month (XX): ");
 		month = scan.nextInt();
-		month -= 1;
+		
 		System.out.print("Enter the year (XXXX): ");
 		year = scan.nextInt();
 		
-		// create calendar with Eastern Standard Time time zone
-		String[] ids = TimeZone.getAvailableIDs(-5 * 60 * 60 * 1000);		
-		 SimpleTimeZone est = new SimpleTimeZone(-5 * 60 * 60 * 1000, ids[0]);		 
-		Calendar calendar = new GregorianCalendar(est);
+		// create a new calendar and set it's date to the year, month, and first day of month
+		Calendar cal = new GregorianCalendar();		
+		//cal.clear();
 		
-		Date currTime = new Date(); // get current date
-		calendar.setTime(currTime);
-		//System.out.println("YEAR: " + calendar.get(Calendar.YEAR));
+		//first month of the year in the Gregorian and Julian calendars is JANUARY which is 0
+		cal.set(year, month-1, 1);
 		
-		// System.out.println("MONTH: " + calendar.get(Calendar.MONTH));		
+		monthStr = cal.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.US).toUpperCase();
+				
+		int firstWeekDay = cal.get(Calendar.DAY_OF_WEEK);
+		//System.out.println("First Week Day: "+firstWeekDay);
 		
+		int lastDayOfMonth = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
+		//System.out.println("Last Day of Month: "+lastDayOfMonth);
 		
-		// create a Gregorian calendar with a particular date
-		// Year must be a four digit integer
-		// Month is an integer from 0 to 11
-		// Day must be an integer from 1 to 31
-		GregorianCalendar gc = new GregorianCalendar(year, month, 1);
-
-		//System.out.println("Month:" + gc.get(Calendar.MONTH));
-		// show the month from the array
-		//System.out.println(gc.get(Calendar.MONTH));
-		System.out.print("\n               " + months[gc.get(Calendar.MONTH)]);
-		System.out.println("Day:" + gc.get(Calendar.DATE));
-		System.out.println(" " + gc.get(Calendar.YEAR));
-		for (int i=0;i<daysOfWeek.length;i++) {
-			System.out.print(daysOfWeek[i]+"  ");
+		System.out.println("\n-----------------------------------");
+		System.out.println("          "+monthStr + " " +year);
+		System.out.println("");
+		System.out.println("  Sun  Mon Tues  Wed Thur  Fri  Sat");
+			
+		// print blank spaces before the first day of the month
+		for (int i=1;i<firstWeekDay;i++) {
+			System.out.print("     ");
 		}
-		int daysInMonth = 30;
-		for (int i=0;i<daysInMonth;i++) {
-			if (i % 7 == 0) {
+		
+		int currWeekDay = firstWeekDay;
+		for (int i=1;i<=lastDayOfMonth;i++) {
+			System.out.printf("   %2d", i);			
+			if (currWeekDay % 7 == 0) {
 				System.out.println("");
-				System.out.print(" ");
 			}
-			System.out.print(String.format("%2d", i)+"    ");
+			currWeekDay++;
 		}
-		
+		System.out.println("\n-----------------------------------");		
 
 	}
-
 }
